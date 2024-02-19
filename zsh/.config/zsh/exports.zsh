9aa02742f7ac222e27beb18e54d292e54621e291
Pedro Augusto <16762743+pedronaugusto@users.noreply.github.com>
2024-02-19 15:28:02 +0000

    brewfile update and vim tutor enabled

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -5,8 +5,6 @@ export PATH="$HOME/.docker/bin":$PATH
 export PATH="$HOME/fvm/default/bin":$PATH
 export PYENV_ROOT="$HOME/.pyenv"
 
-export CHROME_EXECUTABLE="/Applications/Arc.app/Contents/MacOS/Arc"
-
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 
 eval "$(pyenv init -)"
